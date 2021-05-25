798ae26eac23d6a419ccaca8111a3cf5965c851a
Miguel17297 <almeida.m@ua.pt>
2021-05-25 11:50:16 +0100

    add dynamicProperties

--- projService/src/test/java/ua/tqs/deliveryservice/DeliveryServiceApplicationTests.java
+++ projService/src/test/java/ua/tqs/deliveryservice/DeliveryServiceApplicationTests.java
@@ -2,6 +2,8 @@ package ua.tqs.deliveryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -15,6 +17,14 @@ class DeliveryServiceApplicationTests {
 			.withUsername("demo")
 			.withPassword("demopw")
 			.withDatabaseName("delivery");
+
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", container::getJdbcUrl);
+		registry.add("spring.datasource.password", container::getPassword);
+		registry.add("spring.datasource.username", container::getUsername);
+	}
+
 
 
 	@Test
