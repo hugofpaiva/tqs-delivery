5c05c762a859916479c4174930211c9e9297bf00
Miguel17297 <almeida.m@ua.pt>
2021-05-25 11:42:32 +0100

    add testcontainers

--- projService/src/test/java/ua/tqs/deliveryservice/DeliveryServiceApplicationTests.java
+++ projService/src/test/java/ua/tqs/deliveryservice/DeliveryServiceApplicationTests.java
@@ -2,9 +2,20 @@ package ua.tqs.deliveryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class DeliveryServiceApplicationTests {
+
+	@Container
+	public static PostgreSQLContainer container = new PostgreSQLContainer("postgres:11.12")
+			.withUsername("demo")
+			.withPassword("demopw")
+			.withDatabaseName("delivery");
+
 
 	@Test
 	void contextLoads() {
