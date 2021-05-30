d85e463bf604b4fd7a24fe57b466d01c5b0f500e
hugofpaiva <hugofpaiva@ua.pt>
2021-05-30 18:11:29 +0100

    Order Details Modal

--- projClientWeb/src/app/app.module.ts
+++ projClientWeb/src/app/app.module.ts
@@ -1,32 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app.routing';
 
-import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LandingComponent } from './landing/landing.component';
-import {NgbModalRiderReview, ProfileComponent} from './profile/profile.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import {AppComponent} from './app.component';
+import {SignupComponent} from './signup/signup.component';
+import {LandingComponent} from './landing/landing.component';
+import {NgbModalOrderDetails, NgbModalRiderReview, ProfileComponent} from './profile/profile.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {FooterComponent} from './shared/footer/footer.component';
 
-import { HomeModule } from './home/home.module';
-import { LoginComponent } from './login/login.component';
+import {HomeModule} from './home/home.module';
+import {LoginComponent} from './login/login.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    LandingComponent,
-    ProfileComponent,
-    NavbarComponent,
-    FooterComponent,
-    LoginComponent,
-    NgbModalRiderReview
-  ],
+    declarations: [
+        AppComponent,
+        SignupComponent,
+        LandingComponent,
+        ProfileComponent,
+        NavbarComponent,
+        FooterComponent,
+        LoginComponent,
+        NgbModalRiderReview,
+        NgbModalOrderDetails
+    ],
     imports: [
         BrowserModule,
         NgbModule,
@@ -36,7 +37,8 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
         HomeModule,
         FontAwesomeModule
     ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- projClientWeb/src/app/profile/profile.component.ts
+++ projClientWeb/src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {faArrowDown, faStar} from '@fortawesome/free-solid-svg-icons';
+import {faArrowDown, faStar, faMotorcycle, faInfoCircle} from '@fortawesome/free-solid-svg-icons';
 import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -34,20 +34,52 @@ export class NgbModalRiderReview {
 }
 
 @Component({
-    selector: 'app-modal-rider-review',
+    selector: 'app-modal-order-details',
     template: `
         <div class="modal-header">
             <h5 class="modal-title text-center">{{name}}</h5>
         </div>
         <div class="modal-body">
-            <h6 class="mb-0">Order delivered by <b>{{riderName}}</b> at <b>{{orderDate}}</b></h6>
+            <h6 class="mb-0"><fa-icon [icon]="infoIcon"></fa-icon><span style="margin-right: 8px; margin-left: 9px">Status:</span> <b>Delivered</b></h6>
+            <h6 class="mb-0"><fa-icon [icon]="motorcycleIcon"></fa-icon><span style="margin-right: 15px; margin-left: 5px">Rider:</span> <b>{{riderName}} Gonçalves</b></h6>
 
-            <div style="display: flex; justify-content: center; font-size: 2.5rem; margin-top: 10%">
-                <ngb-rating [(rate)]="rate" [max]="max"></ngb-rating>
+            <div class="table" style="min-height: 200px; margin-top: 5%">
+                <table class="table">
+                    <thead class="text-info">
+                    <th>
+                        Product
+                    </th>
+                    <th>
+                        Units
+                    </th>
+                    <th>
+                        Unit Price
+                    </th>
+                    <th>
+                        Total
+                    </th>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            Hammer
+                        </td>
+                        <td>
+                            5
+                        </td>
+                        <td>
+                            5€
+                        </td>
+                        <td>
+                            25€
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <h6 class="mb-0" style="text-align: right;">Total: <b>25€</b></h6>
             </div>
         </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-primary">Add Review</button>
             <button type="button" class="btn btn-link  ml-auto" data-dismiss="modal" (click)="activeModal.dismiss('Close click')">Close
             </button>
         </div>
@@ -57,8 +89,69 @@ export class NgbModalOrderDetails {
     @Input() name;
     @Input() riderName;
     @Input() orderDate;
+    motorcycleIcon = faMotorcycle;
+    infoIcon = faInfoCircle;
     rate = 0;
     max: Number = 5;
+
+    constructor(public activeModal: NgbActiveModal) {
+    }
+}
+
+@Component({
+    selector: 'app-modal-order-details',
+    template: `
+        <div class="modal-header">
+            <h5 class="modal-title text-center">{{name}}</h5>
+        </div>
+        <div class="modal-body">
+            <h6 class="mb-0"><fa-icon [icon]="infoIcon"></fa-icon><span style="margin-right: 8px; margin-left: 9px">Status:</span> <b>Delivered</b></h6>
+            <h6 class="mb-0"><fa-icon [icon]="motorcycleIcon"></fa-icon><span style="margin-right: 15px; margin-left: 5px">Rider:</span> <b>{{riderName}} Gonçalves</b></h6>
+
+            <div class="table" style="min-height: 200px; margin-top: 5%">
+                <table class="table">
+                    <thead class="text-info">
+                    <th>
+                        Product
+                    </th>
+                    <th>
+                        Units
+                    </th>
+                    <th>
+                        Unit Price
+                    </th>
+                    <th>
+                        Total
+                    </th>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            Hammer
+                        </td>
+                        <td>
+                            5
+                        </td>
+                        <td>
+                            5€
+                        </td>
+                        <td>
+                            25€
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <h6 class="mb-0" style="text-align: right;">Total: <b>25€</b></h6>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-link  ml-auto" data-dismiss="modal" (click)="activeModal.dismiss('Close click')">Close
+            </button>
+        </div>
+    `
+})
+export class NgbModalManageAddresses {
+    @Input() name;
 
     constructor(public activeModal: NgbActiveModal) {
     }
@@ -82,6 +175,18 @@ export class ProfileComponent implements OnInit {
         modalRef.componentInstance.name = 'Rider Review';
         modalRef.componentInstance.riderName = 'João';
         modalRef.componentInstance.orderDate = '30-05-2021 15:00PM';
+    }
+
+    openOrderDetails() {
+        const modalRef = this.modalService.open(NgbModalOrderDetails, { centered: true });
+        modalRef.componentInstance.name = 'Order Details';
+        modalRef.componentInstance.riderName = 'João';
+        modalRef.componentInstance.orderDate = '30-05-2021 15:00PM';
+    }
+
+    openManageAddresses() {
+        const modalRef = this.modalService.open(NgbModalOrderDetails, { centered: true });
+        modalRef.componentInstance.name = 'Manage Addresses';
     }
 
     ngOnInit() {
