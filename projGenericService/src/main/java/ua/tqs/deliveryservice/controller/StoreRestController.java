e4f1f5b65214d8d416f243c69cd68b77828602e8
Mariana Santos <marianasps@ua.pt>
2021-06-01 23:50:20 +0100

    receive order

--- projGenericService/src/main/java/ua/tqs/deliveryservice/controller/StoreRestController.java
+++ projGenericService/src/main/java/ua/tqs/deliveryservice/controller/StoreRestController.java
@@ -1,15 +1,80 @@
 package ua.tqs.deliveryservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ua.tqs.deliveryservice.model.Address;
+import ua.tqs.deliveryservice.model.Purchase;
+import ua.tqs.deliveryservice.model.Store;
+import ua.tqs.deliveryservice.repository.AddressRepository;
+import ua.tqs.deliveryservice.repository.PurchaseRepository;
+import ua.tqs.deliveryservice.repository.StoreRepository;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/store")
 public class StoreRestController {
 
+    @Autowired
+    private AddressRepository addressRep;
+
+    @Autowired
+    private PurchaseRepository purchaseRep;
+
+    @Autowired
+    private StoreRepository storeRep;
+
     @PostMapping("/purchase")
-    public ResponseEntity<Object> receivePurchase(@RequestBody String jsonData) {
-        System.out.println("!! " + jsonData + " !!");
-        return null;
+    public ResponseEntity<Object> receivePurchase(@RequestBody Map<String, Map<String, Object>> data) {
+        // System.out.println("!! " + data + " !!");
+
+        /*
+         * { "purchase" : {
+         *          "date" : idkkk,
+         *          "address" : {...}
+         *          "idStore" : 3,
+         *          "clientName" : João
+         *      }
+         * }
+         */
+
+        Map<String, Object> purchase = data.getOrDefault("purchase", null);
+        if (purchase == null) new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        Date date = null;
+        Address address = null;
+        Store store = null;
+        String clientName = null;
+
+        try {
+            date = (Date) purchase.getOrDefault("date", null);
+
+            Object addressMap = purchase.getOrDefault("address", null);
+            objectToAddress(addressMap, address); // <- TODO
+
+            Long idStore = (Long) purchase.getOrDefault("idStore", null);
+            store = storeRep.findById(idStore).get();
+
+            clientName = (String) purchase.getOrDefault("clientName", null);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (date == null || address == null || store == null || clientName == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        addressRep.save(address);
+        Purchase pur = new Purchase(address, store, clientName);
+        purchaseRep.save(pur);
+
+        return null; // TODO: change
+    }
+
+    /* -- helper -- */
+    private void objectToAddress(Object object, Address address) {
+        ;
     }
 }
--- projGenericService/src/main/java/ua/tqs/deliveryservice/model/Purchase.java
+++ projGenericService/src/main/java/ua/tqs/deliveryservice/model/Purchase.java
@@ -35,9 +35,17 @@ public class Purchase {
     @Max(value = 5, message = "Review should not be above the value of 5.")
     private int riderReview;
 
+    // TODO: APAGAR ESTE PQ N FAZ SENTIDO..
     public Purchase(Address address, Rider rider, Store store, String clientName) {
         this.address = address;
         this.rider = rider;
+        this.store = store;
+        this.status = Status.PENDENT;
+        this.clientName = clientName;
+    }
+
+    public Purchase(Address address, Store store, String clientName) {
+        this.address = address;
         this.store = store;
         this.status = Status.PENDENT;
         this.clientName = clientName;
