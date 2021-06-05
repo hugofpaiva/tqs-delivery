fd9298150bbdc9308ac9be66cd192d1ed31ef8cb
hugofpaiva <hugofpaiva@ua.pt>
2021-06-05 01:54:24 +0100

    Enable CSRF

--- projSpecificService/src/main/java/ua/tqs/humberpecas/configuration/WebSecurityConfig.java
+++ projSpecificService/src/main/java/ua/tqs/humberpecas/configuration/WebSecurityConfig.java
@@ -60,7 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
-        httpSecurity.cors().and().csrf().disable()
+        httpSecurity.cors().disable()
                 // dont authenticate this particular request
                 .authorizeRequests()
                 .antMatchers(SWAGGER_WHITELIST).permitAll()
