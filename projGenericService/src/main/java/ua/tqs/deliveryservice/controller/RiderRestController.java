c749f8fd88b5e9730965c7498b34a1838c193099
Mariana Santos <marianasps@ua.pt>
2021-06-07 23:58:21 +0100

    add authorization to update order status

--- projGenericService/src/main/java/ua/tqs/deliveryservice/controller/RiderRestController.java
+++ projGenericService/src/main/java/ua/tqs/deliveryservice/controller/RiderRestController.java
@@ -5,11 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ua.tqs.deliveryservice.model.Person;
 import ua.tqs.deliveryservice.model.Purchase;
 import ua.tqs.deliveryservice.model.Status;
+import ua.tqs.deliveryservice.repository.PersonRepository;
 import ua.tqs.deliveryservice.repository.PurchaseRepository;
 import ua.tqs.deliveryservice.repository.RiderRepository;
+import ua.tqs.deliveryservice.services.JwtUserDetailsService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -22,17 +26,31 @@ public class RiderRestController {
     private RiderRepository riderRep;
 
     @Autowired
+    private PersonRepository personRep;
+
+    @Autowired
     private PurchaseRepository purchaseRep;
 
+    @Autowired
+    private JwtUserDetailsService jwtUserDetailsService;
+
     @PutMapping("order/{order_id}/status")
-    public ResponseEntity<Map<String, Object>> updateOrderStatusAuto(@PathVariable long order_id) {
+    public ResponseEntity<Map<String, Object>> updateOrderStatusAuto(HttpServletRequest request, @PathVariable long order_id) {
+        String requestTokenHeader = request.getHeader("Authorization");
+        String email = jwtUserDetailsService.getEmailFromToken(requestTokenHeader);
+
+        // getting Person who is making the request
+        Person p = personRep.findByEmail(email).orElse(null);
+        if (p == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+
         // get purchase if exists
         Optional<Purchase> pur = purchaseRep.findById(order_id);
         if (pur.isEmpty()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         Purchase purchase = pur.get();
 
-        // todo: check if the authenticated rider is the 'correct'
-        // (needs security implemented)
+        // check if the authenticated rider is the 'correct'
+        if (!purchase.getRider().equals(p)) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
         // gets 'next' status and updates
         Status next = Status.getNext(purchase.getStatus());
