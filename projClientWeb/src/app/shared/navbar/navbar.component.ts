d3eb7a919b0c7b769094fcbd23458cb9584927dc
hugofpaiva <hugofpaiva@ua.pt>
2021-06-12 22:47:31 +0100

    Fix build

--- projClientWeb/src/app/shared/navbar/navbar.component.ts
+++ projClientWeb/src/app/shared/navbar/navbar.component.ts
@@ -18,18 +18,18 @@ export class NavbarComponent implements OnInit {
     userIcon = faUser;
 
     constructor(public location: Location, private router: Router,
-                private accountService: AccountService) {
+                public accountService: AccountService) {
     }
 
     ngOnInit() {
         this.router.events.subscribe((event) => {
             this.isCollapsed = true;
             if (event instanceof NavigationStart) {
-                if (event.url != this.lastPoppedUrl) {
+                if (event.url !== this.lastPoppedUrl) {
                     this.yScrollStack.push(window.scrollY);
                 }
             } else if (event instanceof NavigationEnd) {
-                if (event.url == this.lastPoppedUrl) {
+                if (event.url === this.lastPoppedUrl) {
                     this.lastPoppedUrl = undefined;
                     window.scrollTo(0, this.yScrollStack.pop());
                 } else {
