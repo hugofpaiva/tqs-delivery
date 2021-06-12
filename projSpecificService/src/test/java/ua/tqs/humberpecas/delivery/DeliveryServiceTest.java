2503afdbd690d282f6cb6df0afa3f6f9c39c31ca
Miguel17297 <almeida.m@ua.pt>
2021-06-12 23:49:30 +0100

    begin delivery review tests

---
+++ projSpecificService/src/test/java/ua/tqs/humberpecas/delivery/DeliveryServiceTest.java
@@ -0,0 +1,16 @@
+package ua.tqs.humberpecas.delivery;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DeliveryServiceTest {
+
+    @Test
+    @DisplayName("Add review to Rider")
+    void whenValidReview_sendToDeliveryApp(){
+        
+
+    }
+
+}
