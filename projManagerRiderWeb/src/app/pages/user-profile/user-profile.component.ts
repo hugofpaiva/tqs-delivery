b4c9bc94c28f405b7fcce06c28cafdc972e64281
hugofpaiva <hugofpaiva@ua.pt>
2021-06-13 01:21:49 +0100

    Sidebar based on type of user

--- projManagerRiderWeb/src/app/pages/user-profile/user-profile.component.ts
+++ projManagerRiderWeb/src/app/pages/user-profile/user-profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {faStar} from '@fortawesome/free-solid-svg-icons';
+import {AccountService} from '../../services/account/account.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,9 +11,12 @@ import {faStar} from '@fortawesome/free-solid-svg-icons';
 export class UserProfileComponent implements OnInit {
   starIcon = faStar;
 
-  constructor() { }
+  constructor(private accountService: AccountService, private router: Router) { }
 
   ngOnInit() {
+    if (this.accountService.userValue.type['authority'] === 'Manager') {
+      this.router.navigateByUrl('/stores');
+    }
   }
 
 }
