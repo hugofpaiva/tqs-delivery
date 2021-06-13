9b889e16a3bb0bf9191a95d112f243a802498fd8
hugofpaiva <hugofpaiva@ua.pt>
2021-06-13 01:46:44 +0100

    Fix tests

--- projGenericService/src/main/java/ua/tqs/deliveryservice/controller/AuthController.java
+++ projGenericService/src/main/java/ua/tqs/deliveryservice/controller/AuthController.java
@@ -3,7 +3,6 @@ package ua.tqs.deliveryservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +23,6 @@ public class AuthController {
 
     @Autowired
     private RiderService riderService;
-
-    @Autowired
-    private PasswordEncoder bcryptEncoder;
 
     @PostMapping("/register")
     public ResponseEntity<Rider> registerARider(@RequestBody Map<String, String> payload) {
